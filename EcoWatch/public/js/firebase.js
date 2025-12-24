86cd451d29e0e78467fdd448c878c64f8ef83d23
Mayank Golchha <mayank20044002@gmail.com>
2025-12-24 15:32:18 +0530

    Revise Firebase configuration and imports
    Updated Firebase configuration details.

--- EcoWatch/public/js/firebase.js
+++ EcoWatch/public/js/firebase.js
@@ -7,9 +7,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "APNA FIREBASE KA API",
-  authDomain: "project-948ca.firebaseapp.com",
-  projectId: "project-948ca",
-  storageBucket: "project-948ca.firebasestorage.app",
+  authDomain: "APNA FIREBASE KA PROJECT KA DOMAIN",
+  projectId: "APNA FIREBASE KA PROJECT KA NAAM",
+  storageBucket: "APNA_FIREBASE_KA_PROJECT_KA_NAAM.firebasestorage.app",
   messagingSenderId: "928488170058",
   appId: "1:928488170058:web:07f715bb7ace3bf1343cb3"
 };
@@ -21,3 +21,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export const auth = getAuth(app);
+
